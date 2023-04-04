45128cff59160c1057745dd93a42f842a9cd4c1c
Agustín Morantes <amorantes@itba.edu.ar>
2023-04-03 21:11:05 -0300

    fix: remove old helloWorld mappings

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -1,13 +1,9 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -22,34 +18,5 @@ public class HelloWorldController {
     @RequestMapping("/")
     public ModelAndView home() {
         return new ModelAndView("helloworld/index");
-    }
-
-    @RequestMapping("/hello")
-    public ModelAndView helloWorld() {
-        ModelAndView mav = new ModelAndView("helloworld/hello");
-        mav.addObject("user", userService.createUser("a@a.a", "1234", "A A"));
-        return mav;
-    }
-
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView register(@RequestParam String email, @RequestParam String password, @RequestParam String nombre) {
-        User user = userService.createUser(email, password, nombre);
-        ModelAndView mav = new ModelAndView("helloworld/hello");
-        mav.addObject("user", user);
-
-        return mav;
-    }
-
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public ModelAndView registerForm() {
-        return new ModelAndView("helloworld/register");
-    }
-
-    @RequestMapping("/{id:\\d+}")
-    public ModelAndView profile(@PathVariable long id) {
-        ModelAndView mav = new ModelAndView("helloworld/profile");
-        mav.addObject("userId", id);
-
-        return mav;
     }
 }
