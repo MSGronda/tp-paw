46fd270d8b0889d5eefaea52c9dfed7146ceb41f
Agustín Morantes <amorantes@itba.edu.ar>
2023-04-06 18:46:30 -0300

    feat: Complete schema.sql

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -1,22 +1,58 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id   SERIAL PRIMARY KEY,
-    email    VARCHAR(100) NOT NULL UNIQUE,
-    pass VARCHAR(100) NOT NULL,
-    username VARCHAR(100) NOT NULL
+    id          SERIAL PRIMARY KEY,
+    email       VARCHAR(100) NOT NULL UNIQUE,
+    pass        VARCHAR(100) NOT NULL,
+    username    VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS professor
+CREATE TABLE IF NOT EXISTS professors
 (
-    id SERIAL PRIMARY KEY,
-    profName VARCHAR(100) NOT NULL
+    id          SERIAL PRIMARY KEY,
+    profName    VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS professorSubjects
+CREATE TABLE IF NOT EXISTS subjects
 (
-    idProf SERIAL NOT NULL,
-    idSub SERIAL NOT NULL,
+    id          SERIAL PRIMARY KEY,
+    subName     VARCHAR(100) NOT NULL
+);
 
-    PRIMARY KEY (idProf, idSub),
-    FOREIGN KEY (idProf) REFERENCES professor
+CREATE TABLE IF NOT EXISTS degrees
+(
+    id          SERIAL PRIMARY KEY,
+    degName     VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS review
+(
+    id          SERIAL PRIMARY KEY,
+    idUser      INTEGER NOT NULL REFERENCES users,
+    idSub       INTEGER NOT NULL REFERENCES subjects,
+    score       INTEGER NOT NULL,
+    revText     TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS professorsSubjects
+(
+    idProf  INTEGER NOT NULL REFERENCES professors,
+    idSub   INTEGER NOT NULL REFERENCES subjects,
+
+    PRIMARY KEY (idProf, idSub)
+);
+
+CREATE TABLE IF NOT EXISTS subjectsDegrees
+(
+    idSub   INTEGER NOT NULL REFERENCES subjects,
+    idDeg   INTEGER NOT NULL REFERENCES degrees,
+
+    PRIMARY KEY (idSub, idDeg)
+);
+
+CREATE TABLE IF NOT EXISTS prereqSubjects
+(
+    idSub       INTEGER NOT NULL REFERENCES subjects,
+    idPrereq    INTEGER NOT NULL REFERENCES subjects,
+
+    PRIMARY KEY (idSub, idPrereq)
 );
