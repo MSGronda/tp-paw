0e2406280bba466d3fe0a7b6238b1af560e8abfa
Agustín Morantes <amorantes@itba.edu.ar>
2023-04-10 16:40:01 -0300

    feat: Add hashCode and equals to Degree model

--- models/src/main/java/ar/edu/itba/paw/models/Degree.java
+++ models/src/main/java/ar/edu/itba/paw/models/Degree.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.models;
 
 import java.util.List;
+import java.util.Objects;
 
 
 public class Degree {
@@ -24,5 +25,18 @@ public class Degree {
 
     public List<Long> getSubjectIds() {
         return subjectIds;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Degree degree = (Degree) o;
+        return id == degree.id && name.equals(degree.name) && subjectIds.equals(degree.subjectIds);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
