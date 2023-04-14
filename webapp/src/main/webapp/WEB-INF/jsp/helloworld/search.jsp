e82d2b67eb12ae69b6accc8f2134e72174429d2d
MSGronda <mgronda@itba.edu.ar>
2023-04-14 11:55:10 -0300

    feat: add and remove filters

--- webapp/src/main/webapp/WEB-INF/jsp/helloworld/search.jsp
+++ webapp/src/main/webapp/WEB-INF/jsp/helloworld/search.jsp
@@ -8,7 +8,7 @@
     <style>
         .search-area {
             min-height: 70%; width: 100%;
-            display: flex; flex-direction: column; align-items: center;
+            display: grid;   grid-template-columns: auto auto auto; column-gap: 20px; row-gap: 20px;
             background-color: #f3f3f3; padding-top: 1rem;padding-bottom: 3rem;
         }
         .filter-area {
@@ -27,6 +27,19 @@
             display: flex; flex-direction: column; align-items: center;
             width: 20%;
         }
+        .remove-filter {
+            padding-top: .5rem;
+        }
+        .filter-button {
+            --sl-input-border-width: 0px;
+        }
+        sl-button.filter-button::part(base):hover {
+            background-color: white;
+        }
+
+        .test-card {
+            max-width: 25rem;
+        }
     </style>
 </head>
 <body>
@@ -44,19 +57,51 @@
             <section class="filter-section" id="filter-section">
                 <div class="filter-option">
                     <h5>Department</h5>
+                    <sl-button-group>
+                        <sl-button class="filter-button" size="small" variant="default" id="sistemas-credit-filter" pill>Sistemas Digitales y Datos</sl-button>
+                        <section id="remove-sistemas-param-section">
+                            <sl-button class="filter-button" id="remove-min-credits-filter" variant="default" size="small" pill>
+                                <sl-icon class="remove-filter" name="x-lg" label="Remove"></sl-icon>
+                            </sl-button>
+                        </section>
+                    </sl-button-group>
                 </div>
+
 
                 <div class="filter-option">
                     <h5>Credits</h5>
-                    <sl-button size="small" variant="text" id="min-credit-filter">1 - 3 credits</sl-button>
-                    <sl-button size="small" variant="text" id="med-credit-filter">4 - 6 credits</sl-button>
-                    <sl-button size="small" variant="text" id="max-credit-filter">More than 6 credits</sl-button>
+
+                    <sl-button-group>
+                        <sl-button class="filter-button" size="small" variant="default" id="min-credit-filter" pill>1 - 3 credits</sl-button>
+                        <section id="remove-min-credits-param-section">
+                            <sl-button class="filter-button" id="remove-min-credits-filter" variant="default" size="small" pill>
+                                <sl-icon class="remove-filter" name="x-lg" label="Remove"></sl-icon>
+                            </sl-button>
+                        </section>
+                    </sl-button-group>
+                    <sl-button-group>
+                        <sl-button class="filter-button" size="small" variant="default" id="med-credit-filter" pill>4 - 6 credits</sl-button>
+                        <section id="remove-med-credits-param-section">
+                            <sl-button class="filter-button" id="remove-med-credits-filter" variant="default" size="small" pill>
+                                <sl-icon class="remove-filter" name="x-lg" label="Remove"></sl-icon>
+                            </sl-button>
+                        </section>
+                    </sl-button-group>
+
+                    <sl-button-group>
+                        <sl-button class="filter-button" size="small" variant="default" id="max-credit-filter" pill>More than 6 credits</sl-button>
+                        <section id="remove-max-credits-param-section">
+                            <sl-button class="filter-button" id="remove-max-credits-filter" variant="default" size="small" pill>
+                                <sl-icon class="remove-filter" name="x-lg" label="Remove"></sl-icon>
+                            </sl-button>
+                        </section>
+                    </sl-button-group>
                 </div>
                 <div class="filter-option">
                     <h5>Sort By</h5>
-                    <sl-button size="small" variant="text" id="order-by-name">A-Z</sl-button>
-                    <sl-button size="small" variant="text" id="order-by-credits">Credits</sl-button>
-                    <sl-button size="small" variant="text" id="order-by-id">ID</sl-button>
+                    <sl-button class="filter-button" size="small" variant="default" id="order-by-name">A-Z</sl-button>
+                    <sl-button class="filter-button" size="small" variant="default" id="order-by-credits">Credits</sl-button>
+                    <sl-button class="filter-button" size="small" variant="default" id="order-by-id">ID</sl-button>
                 </div>
             </section>
 
@@ -69,7 +114,7 @@
     <div class="search-area">
 
         <c:forEach var="subject" items="${subjects}">
-            <sl-card class="card-basic">
+            <sl-card class="card-basic test-card">
                 <div slot="header">
                         ${subject.name}
                 </div>
--- webapp/src/main/webapp/js/search-view.js
+++ webapp/src/main/webapp/js/search-view.js
@@ -1,33 +1,90 @@
-// Filter section
-const toggleBtn = document.getElementById('toggle-filters');
-const section = document.getElementById('filter-section');
+// Se corre cada vez que se abre la pagina
 
-toggleBtn.addEventListener('click', function() {
-    if(section.style.display === 'none')
-        section.style.display = 'flex';
-    else
-        section.style.display = 'none';
-});
+let params = new URLSearchParams(window.location.search);
 
-const orderByNameBtn = document.getElementById('order-by-name');
-const orderByCreditsBtn = document.getElementById('order-by-credits');
-const orderByIdBtn = document.getElementById('order-by-id');
+const filterSection = document.getElementById('filter-section');
 
-function findAndReplaceOb(search){
-    if(search.contains("ob=")){
+if(params.keys().next().done) // no tiene parametros
+    filterSection.style.display = "none"
+else
+    filterSection.style.display = "flex"
 
-    }
+const sectionMinCredits = document.getElementById('remove-min-credits-param-section');
+const sectionMedCredits = document.getElementById('remove-med-credits-param-section');
+const sectionMaxCredits = document.getElementById('remove-max-credits-param-section');
+
+
+const sectionDpt = document.getElementById('remove-sistemas-param-section')
+
+let credits = params.get("credits")
+
+sectionMinCredits.style.display = "none";
+sectionMedCredits.style.display = "none";
+sectionMaxCredits.style.display = "none";
+
+switch (credits){
+    case null: break;
+    case "3":
+        sectionMinCredits.style.display = "block";
+        break;
+    case "6": sectionMedCredits.style.display = "block"; break;
+    case "9": sectionMaxCredits.style.display = "block"; break;
 }
+
+let dpt = params.get("department")
+
+sectionDpt.style.display = "none";
+
+switch (dpt){
+    case null: break;
+    case "Sistemas Digitales y Datos": sectionDpt.style.display = "block";
+}
+
 
 function addOrUpdateParam(url, param, value) {
     var regex = new RegExp("([?&])" + param + "=.*?(&|$)", "i");
     var separator = url.indexOf('?') !== -1 ? "&" : "?";
     if (url.match(regex)) {
         return url.replace(regex, '$1' + param + "=" + value + '$2');
-    } else {
-        return url + separator + param + "=" + value;
     }
+    else
+        return url + separator + param + "=" + value;
 }
+
+function removeURLParam(url, param) {
+    var urlsections = url.split('?');
+
+    if (urlsections.length >= 2) {
+        var prefix = encodeURIComponent(param) + '=';
+        var params = urlsections[1].split(/[&;]/g);
+
+        for (var i = params.length - 1; i >= 0; i--) {
+            if (decodeURIComponent(params[i]).lastIndexOf(prefix, 0) !== -1) {
+                params.splice(i, 1);
+            }
+        }
+        url = urlsections[0] + (params.length > 0 ? '?' + params.join('&') : '');
+        return url;
+    }
+    else
+        return url;
+
+}
+
+// Filter section
+const toggleBtn = document.getElementById('toggle-filters');
+
+toggleBtn.addEventListener('click', function() {
+    if(filterSection.style.display === 'none')
+        filterSection.style.display = 'flex';
+    else
+        filterSection.style.display = 'none';
+});
+
+const orderByNameBtn = document.getElementById('order-by-name');
+const orderByCreditsBtn = document.getElementById('order-by-credits');
+const orderByIdBtn = document.getElementById('order-by-id');
+
 
 // Order by section
 //TODO unificar todas las function() en una
@@ -46,10 +103,11 @@ orderByIdBtn.addEventListener('click', function() {
 });
 
 
+// Credit filters
+
 const minCreditFilterBtn = document.getElementById('min-credit-filter');
 const medCreditFilterBtn = document.getElementById('med-credit-filter');
 const maxCreditFilterBtn = document.getElementById('max-credit-filter');
-
 
 minCreditFilterBtn.addEventListener('click', function() {
     window.location.href = addOrUpdateParam(window.location.href,"credits","3");
@@ -62,4 +120,38 @@ medCreditFilterBtn.addEventListener('click', function() {
 maxCreditFilterBtn.addEventListener('click', function() {
     window.location.href = addOrUpdateParam(window.location.href,"credits","9");
 });
+
+
+const removeMinCreditFilterBtn = document.getElementById('remove-min-credits-filter');
+const removeMedCreditFilterBtn = document.getElementById('remove-med-credits-filter');
+const removeMaxCreditFilterBtn = document.getElementById('remove-max-credits-filter');
+
+
+removeMinCreditFilterBtn.addEventListener('click', function() {
+    window.location.href = removeURLParam(window.location.href,"credits");
+});
+
+removeMedCreditFilterBtn.addEventListener('click', function() {
+    window.location.href = removeURLParam(window.location.href,"credits");
+});
+
+removeMaxCreditFilterBtn.addEventListener('click', function() {
+    window.location.href = removeURLParam(window.location.href,"credits");
+});
+
+
+// Department filters
+
+const sistemasFilterBtn = document.getElementById('sistemas-credit-filter');
+sistemasFilterBtn.addEventListener('click', function() {
+    window.location.href = addOrUpdateParam(window.location.href,"department","Sistemas Digitales y Datos");
+});
+
+const removeSistemasFilter = document.getElementById('remove-sistemas-param-section');
+removeSistemasFilter.addEventListener('click', function() {
+    window.location.href = removeURLParam(window.location.href,"department");
+});
+
+
+
 
