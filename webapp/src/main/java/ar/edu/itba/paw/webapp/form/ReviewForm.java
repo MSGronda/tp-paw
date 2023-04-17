0e7e08a9c885796aa03057598197608db99f255c
Agustín Morantes <amorantes@itba.edu.ar>
2023-04-17 00:36:30 -0300

    fix: change email annotation import

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/ReviewForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/ReviewForm.java
@@ -1,6 +1,6 @@
 package ar.edu.itba.paw.webapp.form;
 
-import javax.validation.constraints.Email;
+import org.hibernate.validator.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,6 +9,7 @@ public class ReviewForm {
     @Email
     @Size(min = 1)
     private String email;
+
     @Size(min = 1)
     private String text;
 
