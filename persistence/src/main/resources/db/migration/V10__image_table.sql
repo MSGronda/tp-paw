a44cd54480426ad7dca199e8709d71c86703ff1d
Agustín Morantes <amorantes@itba.edu.ar>
2023-05-10 19:34:03 -0300

    fix: use standard SQL syntax for updating users table

--- persistence/src/main/resources/db/migration/V10__image_table.sql
+++ persistence/src/main/resources/db/migration/V10__image_table.sql
@@ -9,9 +9,11 @@ ALTER TABLE users ADD COLUMN image_id INTEGER REFERENCES images(id);
 INSERT INTO images (image)
 SELECT image FROM users;
 
-UPDATE users SET image_id = images.id
-FROM images
-WHERE images.image = users.image;
+UPDATE users u SET image_id = (
+    SELECT i.id
+    FROM images i
+    WHERE u.image = i.image
+);
 
 
 ALTER TABLE users DROP COLUMN image;
