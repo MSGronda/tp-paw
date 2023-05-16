9e4a24c066917c2f7a873f95f1ac250789371a76
mrojaspell <mrojaspelliccia@itba.edu.ar>
2023-05-16 12:16:34 -0300

    fix: utils cannot be instantiated

--- webapp/src/main/java/ar/edu/itba/paw/webapp/Utils.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/Utils.java
@@ -3,6 +3,8 @@ package ar.edu.itba.paw.webapp;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 public class Utils {
+    private Utils(){}
+
     public static String getBaseUrl() {
         return ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
     }
