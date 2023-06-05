5c7aec88e11b7f9ecb9cd63e4b9518c0476c6ec9
Agustín Morantes <amorantes@itba.edu.ar>
2023-06-05 19:49:43 -0300

    refactor: remove ExceptionControllerAdvice

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/DegreeNotFoundException.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/DegreeNotFoundException.java
@@ -1,5 +1,9 @@
 package ar.edu.itba.paw.webapp.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class DegreeNotFoundException extends RuntimeException{
     public DegreeNotFoundException() {
         super();
