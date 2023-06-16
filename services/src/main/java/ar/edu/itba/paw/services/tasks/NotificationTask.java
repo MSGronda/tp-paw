398d6330f514286a36a949ef0c4583b2b97c905f
Agustín Morantes <amorantes@itba.edu.ar>
2023-06-16 16:26:38 -0300

    feat: move notification task to services

--- services/src/main/java/ar/edu/itba/paw/services/tasks/NotificationTask.java
+++ services/src/main/java/ar/edu/itba/paw/services/tasks/NotificationTask.java
@@ -1,4 +1,4 @@
-package ar.edu.itba.paw.webapp.tasks;
+package ar.edu.itba.paw.services.tasks;
 
 import ar.edu.itba.paw.models.Subject;
 import ar.edu.itba.paw.models.User;
@@ -7,31 +7,24 @@ import ar.edu.itba.paw.services.SubjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.MessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
 @Component
 public class NotificationTask {
     private static final long TASK_DELAY = 10 * 60 * 1000L; // 10 minutes
-
     private static final Logger LOGGER = LoggerFactory.getLogger(NotificationTask.class);
 
     private final MailService mailService;
     private final SubjectService subjectService;
-    private final String baseUrl;
 
     @Autowired
     NotificationTask(final MailService mailService, final SubjectService subjectService, final Environment env) {
         this.mailService = mailService;
         this.subjectService = subjectService;
-        this.baseUrl = env.getRequiredProperty("baseUrl");
     }
 
     //@Scheduled(fixedDelay = TASK_DELAY)
