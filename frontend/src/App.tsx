fbaf8437af2ae9cbc4e497620a4273555f9cb75b
Agustín Morantes <amorantes@itba.edu.ar>
2023-08-06 12:30:16 -0300

    feat: frontend scaffolding

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { MantineProvider } from '@mantine/core';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+
+export default function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <RouterProvider router={router}/>
+    </MantineProvider>
+  );
+}
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import { Text } from '@mantine/core';
+
+export default function Home() {
+    return (
+        <Text>Welcome to Mantine!</Text>
+    );
+}
