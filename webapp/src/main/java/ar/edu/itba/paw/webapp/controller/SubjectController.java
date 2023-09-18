d19487ef458eea45c0d9be05b165fc2abb186330
MSGronda <mgronda@itba.edu.ar>
2023-09-18 16:29:36 -0300

    Revert "feat: initial subject api"
    This reverts commit ab42693ff96468859b6ad8d8507e9c73ebbb2a71.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/SubjectController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/SubjectController.java
@@ -1,37 +1,31 @@
 package ar.edu.itba.paw.webapp.controller;
 
-
+import ar.edu.itba.paw.models.Degree;
+import ar.edu.itba.paw.models.Professor;
 import ar.edu.itba.paw.models.Subject;
+import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.models.exceptions.SubjectNotFoundException;
+import ar.edu.itba.paw.models.exceptions.UnauthorizedException;
 import ar.edu.itba.paw.services.*;
-import ar.edu.itba.paw.webapp.dto.SubjectDto;
+import ar.edu.itba.paw.models.exceptions.SubjectIdAlreadyExistsException;
+import ar.edu.itba.paw.webapp.form.EditSubjectForm;
+import ar.edu.itba.paw.webapp.form.SubjectForm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.*;
+import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
-
-@Path("subjects")
-@Component
+@Controller
 public class SubjectController {
-
     private final SubjectService subjectService;
-
-
     private final ReviewService reviewService;
-
-
     private final DegreeService degreeService;
-
     private final AuthUserService authUserService;
-
-
     private final ProfessorService professorService;
-
-    @Context
-    private UriInfo uriInfo;
 
     @Autowired
     public SubjectController(
@@ -48,155 +42,135 @@ public class SubjectController {
         this.professorService = professorService;
     }
 
+    @RequestMapping("/subject/{id:\\d+\\.\\d+}")
+    public ModelAndView subjectInfo(
+            @PathVariable final String id,
+            @RequestParam(name = "page", defaultValue = "1") final int page,
+            @RequestParam(defaultValue = "name") final String order,
+            @RequestParam(defaultValue = "asc") final String dir
+    ) {
+        final Subject subject = subjectService.findById(id).orElseThrow(SubjectNotFoundException::new);
+        final User user = authUserService.getCurrentUser();
 
+        final ModelAndView mav = new ModelAndView("subjects/subject_info");
+        mav.addObject("totalPages", reviewService.getTotalPagesForSubjectReviews(subject));
+        mav.addObject("reviews", reviewService.getAllSubjectReviews(subject, page, order, dir));
+        mav.addObject("degree", degreeService.findParentDegree(subject, user));
+        mav.addObject("year", degreeService.findSubjectYearForParentDegree(subject, user));
+        mav.addObject("didReview", reviewService.didUserReview(subject, user));
+        mav.addObject("progress", user.getSubjectProgress(subject));
+        mav.addObject("user", user);
+        mav.addObject("subject", subject);
+        mav.addObject("currentPage", page);
+        mav.addObject("order", order);
+        mav.addObject("dir", dir);
+        return mav;
+    }
 
-//    @RequestMapping("/subject/{id:\\d+\\.\\d+}")
-//    public ModelAndView subjectInfo(
-//            @PathVariable final String id,
-//            @RequestParam(name = "page", defaultValue = "1") final int page,
-//            @RequestParam(defaultValue = "name") final String order,
-//            @RequestParam(defaultValue = "asc") final String dir
-//    ) {
-//        final Subject subject = subjectService.findById(id).orElseThrow(SubjectNotFoundException::new);
-//        final User user = authUserService.getCurrentUser();
-//
-//        final ModelAndView mav = new ModelAndView("subjects/subject_info");
-//        mav.addObject("totalPages", reviewService.getTotalPagesForSubjectReviews(subject));
-//        mav.addObject("reviews", reviewService.getAllSubjectReviews(subject, page, order, dir));
-//        mav.addObject("degree", degreeService.findParentDegree(subject, user));
-//        mav.addObject("year", degreeService.findSubjectYearForParentDegree(subject, user));
-//        mav.addObject("didReview", reviewService.didUserReview(subject, user));
-//        mav.addObject("progress", user.getSubjectProgress(subject));
-//        mav.addObject("user", user);
-//        mav.addObject("subject", subject);
-//        mav.addObject("currentPage", page);
-//        mav.addObject("order", order);
-//        mav.addObject("dir", dir);
-//        return mav;
-//    }
-//
-//    @RequestMapping(value = "/create-subject", method = {RequestMethod.GET})
-//    public ModelAndView createSubjectForm(@ModelAttribute("subjectForm") final SubjectForm subjectForm) {
-//        final List<Subject> subjects = subjectService.getAll();
-//        final List<Professor> professors = professorService.getAll();
-//        final List<Degree> degrees = degreeService.getAll();
-//
-//        final ModelAndView mav = new ModelAndView("moderator-tools/createSubject");
-//        mav.addObject("subjects", subjects);
-//        mav.addObject("professors", professors);
-//        mav.addObject("degrees", degrees);
-//        return mav;
-//    }
-//
-//    @RequestMapping(value = "/create-subject", method = {RequestMethod.POST} )
-//    public ModelAndView createSubject(
-//            @Valid @ModelAttribute("subjectForm") final SubjectForm subjectForm,
-//            final BindingResult errors
-//    ) {
-//        if(errors.hasErrors()) {
-//            return createSubjectForm(subjectForm);
-//        }
-//
-//        final Subject newSub;
-//        try{
-//            newSub = subjectService.create(Subject.builder()
-//                    .id(subjectForm.getId())
-//                    .name(subjectForm.getName())
-//                    .department(subjectForm.getDepartment())
-//                    .credits(subjectForm.getCredits()),
-//                    subjectForm.getDegreeIds(),
-//                    subjectForm.getSemesters(),
-//                    subjectForm.getRequirementIds(),
-//                    subjectForm.getProfessors(),
-//                    subjectForm.getClassCodes(),
-//                    subjectForm.getClassProfessors(),
-//                    subjectForm.getClassDays(),
-//                    subjectForm.getClassStartTimes(),
-//                    subjectForm.getClassEndTimes(),
-//                    subjectForm.getClassBuildings(),
-//                    subjectForm.getClassRooms(),
-//                    subjectForm.getClassModes()
-//            );
-//        } catch (SubjectIdAlreadyExistsException e) {
-//            final ModelAndView mav = createSubjectForm(subjectForm);
-//            mav.addObject("subjectCodeRepeated", true);
-//            return mav;
-//        }
-//        return new ModelAndView("redirect:/subject/"+newSub.getId());
-//    }
-//
-//    @RequestMapping("/subject/{id:\\d+\\.\\d+}/delete-subject")
-//    public ModelAndView deleteSubject(
-//            @PathVariable final String id
-//    ) throws UnauthorizedException, SubjectNotFoundException {
-//
-//        subjectService.delete(authUserService.getCurrentUser(), id);
-//        return new ModelAndView("redirect:/");
-//    }
-//
-//    @RequestMapping(value = "/subject/{id:\\d+\\.\\d+}/edit", method = {RequestMethod.GET})
-//    public ModelAndView editSubjectForm(
-//            @PathVariable final String id,
-//            @ModelAttribute("subjectForm") final EditSubjectForm editSubjectForm
-//    ) {
-//        final Subject subject = subjectService.findById(id).orElseThrow(SubjectNotFoundException::new);
-//        final List<Subject> subjects = subjectService.getAll();
-//        final List<Professor> professors = professorService.getAll();
-//        final List<Degree> degrees = degreeService.getAll();
-//
-//        final ModelAndView mav = new ModelAndView("moderator-tools/editSubject");
-//        mav.addObject("subject", subject);
-//        mav.addObject("allSubjects", subjects);
-//        mav.addObject("professors", professors);
-//        mav.addObject("degrees", degrees);
-//        return mav;
-//    }
-//
-//    @RequestMapping(value = "/subject/{id:\\d+\\.\\d+}/edit", method = {RequestMethod.POST})
-//    public ModelAndView editSubject(
-//            @PathVariable final String id,
-//            @Valid @ModelAttribute("editSubjectForm") final EditSubjectForm editSubjectForm,
-//            final BindingResult errors
-//    ) {
-//        if(errors.hasErrors()) {
-//            return editSubjectForm(id, editSubjectForm);
-//        }
-//
-//        subjectService.edit(
-//                id,
-//                editSubjectForm.getCredits(),
-//                editSubjectForm.getDegreeIds(),
-//                editSubjectForm.getSemesters(),
-//                editSubjectForm.getRequirementIds(),
-//                editSubjectForm.getProfessors(),
-//                editSubjectForm.getClassIds(),
-//                editSubjectForm.getClassCodes(),
-//                editSubjectForm.getClassProfessors(),
-//                editSubjectForm.getClassDays(),
-//                editSubjectForm.getClassStartTimes(),
-//                editSubjectForm.getClassEndTimes(),
-//                editSubjectForm.getClassBuildings(),
-//                editSubjectForm.getClassRooms(),
-//                editSubjectForm.getClassModes()
-//        );
-//        return new ModelAndView("redirect:/subject/"+id);
-//    }
+    @RequestMapping(value = "/create-subject", method = {RequestMethod.GET})
+    public ModelAndView createSubjectForm(@ModelAttribute("subjectForm") final SubjectForm subjectForm) {
+        final List<Subject> subjects = subjectService.getAll();
+        final List<Professor> professors = professorService.getAll();
+        final List<Degree> degrees = degreeService.getAll();
 
+        final ModelAndView mav = new ModelAndView("moderator-tools/createSubject");
+        mav.addObject("subjects", subjects);
+        mav.addObject("professors", professors);
+        mav.addObject("degrees", degrees);
+        return mav;
+    }
 
+    @RequestMapping(value = "/create-subject", method = {RequestMethod.POST} )
+    public ModelAndView createSubject(
+            @Valid @ModelAttribute("subjectForm") final SubjectForm subjectForm,
+            final BindingResult errors
+    ) {
+        if(errors.hasErrors()) {
+            return createSubjectForm(subjectForm);
+        }
 
-    // = = = = = = = = = = = API = = = = = = = = = = = = =
-    @GET
-    @Produces("application/vnd.subject-list.v1+json")
-    public Response getSubjects(
-            // PARAMS
-    ){
-        List<Subject> subs = subjectService.getAll();
-        List<SubjectDto> subjectsDtos = subs.stream().map(subject -> SubjectDto.fromSubject(uriInfo, subject)).collect(Collectors.toList());
+        final Subject newSub;
+        try{
+            newSub = subjectService.create(Subject.builder()
+                    .id(subjectForm.getId())
+                    .name(subjectForm.getName())
+                    .department(subjectForm.getDepartment())
+                    .credits(subjectForm.getCredits()),
+                    subjectForm.getDegreeIds(),
+                    subjectForm.getSemesters(),
+                    subjectForm.getRequirementIds(),
+                    subjectForm.getProfessors(),
+                    subjectForm.getClassCodes(),
+                    subjectForm.getClassProfessors(),
+                    subjectForm.getClassDays(),
+                    subjectForm.getClassStartTimes(),
+                    subjectForm.getClassEndTimes(),
+                    subjectForm.getClassBuildings(),
+                    subjectForm.getClassRooms(),
+                    subjectForm.getClassModes()
+            );
+        } catch (SubjectIdAlreadyExistsException e) {
+            final ModelAndView mav = createSubjectForm(subjectForm);
+            mav.addObject("subjectCodeRepeated", true);
+            return mav;
+        }
+        return new ModelAndView("redirect:/subject/"+newSub.getId());
+    }
 
-        if(subjectsDtos.isEmpty())
-            return Response.noContent().build();
+    @RequestMapping("/subject/{id:\\d+\\.\\d+}/delete-subject")
+    public ModelAndView deleteSubject(
+            @PathVariable final String id
+    ) throws UnauthorizedException, SubjectNotFoundException {
 
-        Response.ResponseBuilder responseBuilder = Response.ok(new GenericEntity<List<SubjectDto>>(subjectsDtos){});
-        return responseBuilder.build();
+        subjectService.delete(authUserService.getCurrentUser(), id);
+        return new ModelAndView("redirect:/");
+    }
+
+    @RequestMapping(value = "/subject/{id:\\d+\\.\\d+}/edit", method = {RequestMethod.GET})
+    public ModelAndView editSubjectForm(
+            @PathVariable final String id,
+            @ModelAttribute("subjectForm") final EditSubjectForm editSubjectForm
+    ) {
+        final Subject subject = subjectService.findById(id).orElseThrow(SubjectNotFoundException::new);
+        final List<Subject> subjects = subjectService.getAll();
+        final List<Professor> professors = professorService.getAll();
+        final List<Degree> degrees = degreeService.getAll();
+
+        final ModelAndView mav = new ModelAndView("moderator-tools/editSubject");
+        mav.addObject("subject", subject);
+        mav.addObject("allSubjects", subjects);
+        mav.addObject("professors", professors);
+        mav.addObject("degrees", degrees);
+        return mav;
+    }
+
+    @RequestMapping(value = "/subject/{id:\\d+\\.\\d+}/edit", method = {RequestMethod.POST})
+    public ModelAndView editSubject(
+            @PathVariable final String id,
+            @Valid @ModelAttribute("editSubjectForm") final EditSubjectForm editSubjectForm,
+            final BindingResult errors
+    ) {
+        if(errors.hasErrors()) {
+            return editSubjectForm(id, editSubjectForm);
+        }
+
+        subjectService.edit(
+                id,
+                editSubjectForm.getCredits(),
+                editSubjectForm.getDegreeIds(),
+                editSubjectForm.getSemesters(),
+                editSubjectForm.getRequirementIds(),
+                editSubjectForm.getProfessors(),
+                editSubjectForm.getClassIds(),
+                editSubjectForm.getClassCodes(),
+                editSubjectForm.getClassProfessors(),
+                editSubjectForm.getClassDays(),
+                editSubjectForm.getClassStartTimes(),
+                editSubjectForm.getClassEndTimes(),
+                editSubjectForm.getClassBuildings(),
+                editSubjectForm.getClassRooms(),
+                editSubjectForm.getClassModes()
+        );
+        return new ModelAndView("redirect:/subject/"+id);
     }
 }
