f1925bbab4e9feabf213fa000014ee2c28fd4ba9
mrojaspell <mrojaspelliccia@itba.edu.ar>
2023-10-13 15:12:05 -0300

    feat: degreeDto

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/DegreeDto.java
@@ -0,0 +1,44 @@
+package ar.edu.itba.paw.webapp.dto;
+
+import ar.edu.itba.paw.models.Degree;
+
+import javax.ws.rs.core.UriInfo;
+
+public class DegreeDto {
+
+    private Long id;
+    private String name;
+
+    private Integer totalCredits;
+
+    public static DegreeDto fromDegree(UriInfo uriInfo, Degree degree){
+        DegreeDto degreeDto = new DegreeDto();
+
+        degreeDto.id = degree.getId();
+        degreeDto.name = degree.getName();
+        degreeDto.totalCredits = degree.getTotalCredits();
+
+        return degreeDto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(){
+        this.name = name;
+    }
+    public Integer getTotalCredits() {
+        return totalCredits;
+    }
+    public void setTotalCredits(){
+        this.totalCredits = totalCredits;
+    }
+
+}
