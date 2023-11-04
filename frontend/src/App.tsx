10424343490029c3d4a0f93f86aa5086f37fd7aa
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-11-04 10:49:06 -0300

    feat: mantine version changed for compatability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { MantineProvider } from '@mantine/core';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+// core styles are required for all packages
+import '@mantine/core/styles.css';
 
 export default function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider>
       <RouterProvider router={router}/>
     </MantineProvider>
   );
