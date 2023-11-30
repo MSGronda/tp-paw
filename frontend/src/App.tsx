efcb9f1b121a08b653964308698bfd4ec6bdb098
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-11-30 15:56:51 -0300

    feat: i18n change based on browser settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,17 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 // core styles are required for all packages
 import '@mantine/core/styles.css';
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const { i18n } = useTranslation();
+  useEffect(() => {
+    const lng = navigator.language;
+    i18n.changeLanguage(lng);
+  }, []);
+  
   return (
     <MantineProvider>
       <RouterProvider router={router}/>
