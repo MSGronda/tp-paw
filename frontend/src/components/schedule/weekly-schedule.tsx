163851e4ab67235b59fa683af921eadff5779aa1
MSGronda <mgronda@itba.edu.ar>
2024-01-13 12:17:47 -0300

    feat: weekly schedule

--- frontend/src/components/schedule/weekly-schedule.tsx
+++ frontend/src/components/schedule/weekly-schedule.tsx
@@ -1,28 +1,94 @@
 import {t} from "i18next";
 import "../../common/table-style.css";
-import {useState} from "react";
+import ClassTime from "../../models/ClassTime.ts";
+
 interface WeeklyScheduleProps {
     rows: number;
     cols: number;
+
+    // Hay para cada classTime, hay un subject name
+    subjectNames: string[];
+    classTimes: ClassTime[];
+}
+
+const HIDDEN_CELL = -1;
+const EMPTY_CELL = -2;
+
+function getRowIndex(time: string){
+    if(time === '')
+        return -1
+    return (parseInt(time.split(":")[0]) - 8)*2 + (parseInt(time.split(":")[1]) === 30 ? 1 : 0);
+}
+function calcRowSpan(classTime: ClassTime): number {
+    return getRowIndex(classTime.endTime) - getRowIndex(classTime.startTime);
+}
+function findEventByIdx(rowIdx: number, colIdx: number, classTimes: ClassTime[]): number{
+    for(let i=0; i<classTimes.length; i++){
+        if(colIdx + 1 == classTimes[i].day){
+            const startTimeIdx = getRowIndex(classTimes[i].startTime);
+            const endTimeIdx = getRowIndex(classTimes[i].endTime);
+            if(rowIdx == startTimeIdx){
+                return i;
+            }
+            else if(startTimeIdx < rowIdx && rowIdx <= endTimeIdx){
+                return HIDDEN_CELL;
+            }
+            return EMPTY_CELL;
+        }
+    }
+    return EMPTY_CELL;
+}
+
+function generateTimeSlots(rows: number): string[] {
+    const resp: string[] = [];
+    for(let i=0; i<rows; i++){
+        const hours = (8 + Math.floor(i / 2)) + "";
+        const minutes = i % 2 === 0 ? "00" : "30";
+        resp.push(hours + ":" + minutes);
+    }
+    return resp;
 }
 
 export default function WeeklySchedule(props: WeeklyScheduleProps) {
-    const {rows, cols} = props;
-
+    const {rows, cols, subjectNames, classTimes} = props;
     const colors = [
         '#fda4a5', '#fdba74' ,'#fce046', '#bff265',
         '#86eead', '#5febd4', '#7dd2fd', '#a4b5fc'
     ]
+    const timeSlots = generateTimeSlots(rows);
+    let currentColor = 1;
 
-    const [currentColor, setCurrentColor] = useState(0);
-    const [schedule, setSchedule] = useState<string[]>(Array.from({ length: rows * cols }, () => ""));
+    const getNextColor = () => {
+        currentColor = (currentColor + 1) % colors.length
+        return colors[currentColor];
+    };
 
-    const updateScheduleByIdx = (index: number, newContent: string) => {
-        setSchedule((prevElements) => {
-            const newElements = [...prevElements];
-            newElements[index] = newContent;
-            return newElements;
-        });
+    const renderTableCells = (rowIdx: number, colIdx: number) => {
+        const idx = findEventByIdx(rowIdx, colIdx, classTimes);
+        if(idx>=0){
+            const cellColor = getNextColor();
+            return (
+                <td rowSpan={calcRowSpan(classTimes[idx])} style={{backgroundColor: cellColor}}>
+                    <div style={{maxWidth: "6rem", textAlign: "center", alignSelf: "center", fontWeight: "bold"}}>
+                        {subjectNames[idx]}
+                    </div>
+                    <div style={{textAlign: "center"}}>{classTimes[idx].classNumber}</div>
+                    <div style={{textAlign: "center"}}>{classTimes[idx].mode}</div>
+                </td>
+            );
+        }
+        else if(idx==HIDDEN_CELL){
+            return <td style={{display: "none"}}></td>;
+        }
+        return <td></td>;
+    };
+    const renderTableRow = (time: string, rowIdx: number) => {
+        return (
+            <tr>
+                <th>{time}</th>
+                {Array.from({ length: cols - 1 }, (_, colIdx) => renderTableCells(rowIdx, colIdx))}
+            </tr>
+        );
     };
 
     return (
@@ -38,8 +104,8 @@ export default function WeeklySchedule(props: WeeklyScheduleProps) {
                 <th>{t("TimeTable.day6")}</th>
             </tr>
             </thead>
-            <tbody id="weekly-schedule">
-                {}
+            <tbody>
+                {timeSlots.map((time, index)=> renderTableRow(time, index))}
             </tbody>
         </table>
     )
--- frontend/src/pages/Test/test.tsx
+++ frontend/src/pages/Test/test.tsx
@@ -3,11 +3,38 @@ import "../../common/i18n/index";
 import WeeklySchedule from "../../components/schedule/weekly-schedule.tsx";
 
 export default function Test() {
+    const classTimes = [
+        {
+            day: 1,
+            startTime: "8:00",
+            endTime: "10:00",
+            classNumber: "101F",
+            building: "SDF",
+            mode: "Presencial"
+        },
+        {
+            day: 5,
+            startTime: "11:00",
+            endTime: "22:00",
+            classNumber: "101F",
+            building: "SDF",
+            mode: "Presencial"
+        }
+    ];
+    const subjectNames = [
+        "Cacona 1",
+        "Cacona 1",
+    ]
 
     return (
         <div className={classes.fullsize}>
                 <div style={{width: 600, height: 900}}>
-                    <WeeklySchedule/>
+                    <WeeklySchedule
+                        rows={29}
+                        cols={7}
+                        classTimes={classTimes}
+                        subjectNames={subjectNames}
+                    />
                 </div>
         </div>
     )
