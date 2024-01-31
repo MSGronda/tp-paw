9e55ef2994f2e672f34af483528f7e76368bb431
Agustín Morantes <amorantes@itba.edu.ar>
2024-01-31 13:33:36 -0300

    feat(api): add email confirmation endpoint

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ConfirmController.java
@@ -0,0 +1,31 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.models.exceptions.InvalidTokenException;
+import ar.edu.itba.paw.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("verification-token")
+@Component
+public class ConfirmController {
+    @Autowired
+    private UserService userService;
+    
+    @POST
+    public Response confirm(@RequestBody String token) {
+        try {
+            userService.confirmUser(token);
+            return Response.ok().build();
+        } catch (InvalidTokenException e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+}
