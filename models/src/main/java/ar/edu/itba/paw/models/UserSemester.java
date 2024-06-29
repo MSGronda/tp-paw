a01733e37b54a330809883839eb4f00f0696e46c
MSGronda <mgronda@itba.edu.ar>
2024-06-29 15:25:18 -0300

    fix: user semester SQL definition

--- models/src/main/java/ar/edu/itba/paw/models/UserSemester.java
+++ models/src/main/java/ar/edu/itba/paw/models/UserSemester.java
@@ -9,18 +9,20 @@ import java.util.Objects;
 @Entity
 @Table(name = "userSemester")
 public class UserSemester {
-    @EmbeddedId
-    private Key key;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "usersemester_id_seq")
+    @SequenceGenerator(sequenceName = "usersemester_id_seq", name = "usersemester_id_seq", allocationSize = 1)
+    private Long id;
 
     @ManyToOne(optional = false)
-    @MapsId("iduser")
     @JoinColumn(name = "iduser")
     private User user;
 
     @ManyToOne(optional = false)
     @JoinColumns({
-            @JoinColumn(name = "idsub", referencedColumnName = "idsub", insertable = false, updatable = false), // TODO: POR FAVOR CHEQUEA ESTO
-            @JoinColumn(name = "idclass", referencedColumnName = "idclass", insertable = false, updatable = false) // TODO: POR FAVOR CHEQUEA ESTO
+            @JoinColumn(name = "idsub", referencedColumnName = "idsub"),
+            @JoinColumn(name = "idclass", referencedColumnName = "idclass")
     })
     private SubjectClass subjectClass;
 
@@ -29,18 +31,26 @@ public class UserSemester {
 
     protected UserSemester() {}
 
-    public UserSemester(final User user, final SubjectClass subjectClass, final Timestamp dateFinished) {
+    public UserSemester(final Long id, final User user, final SubjectClass subjectClass, final Timestamp dateFinished) {
+        this.id = id;
         this.user = user;
         this.subjectClass = subjectClass;
         this.dateFinished = dateFinished;
-        this.key = new UserSemester.Key(user.getId(), subjectClass.getClassId(), subjectClass.getSubject().getId());
     }
 
     public UserSemester(final User user, final SubjectClass subjectClass) {
         this.user = user;
         this.subjectClass = subjectClass;
         this.dateFinished = null;
-        this.key = new UserSemester.Key(user.getId(), subjectClass.getClassId(), subjectClass.getSubject().getId());
+    }
+
+    public boolean isActive(){
+        return dateFinished == null;
+
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public User getUser() {
@@ -55,10 +65,9 @@ public class UserSemester {
         return dateFinished;
     }
 
-    public boolean isActive(){
-        return dateFinished == null;
+    public void setId(Long id) {
+        this.id = id;
     }
-
     public void setDateFinished(Timestamp dateFinished) {
         this.dateFinished = dateFinished;
     }
@@ -76,56 +85,11 @@ public class UserSemester {
         if (this == o) return true;
         if (!(o instanceof UserSemester)) return false;
         UserSemester that = (UserSemester) o;
-        return Objects.equals(key, that.key);
+        return Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(key);
-    }
-
-    @Embeddable
-    public static class Key implements Serializable {
-        @Column(name = "iduser")
-        private Long iduser;
-
-        @Column(name = "idclass")
-        private String idclass;
-
-        @Column(name = "idsub")
-        private String idsub;
-
-        public Key(final Long idUser, final String idClass, final String idSub) {
-            this.iduser = idUser;
-            this.idclass = idClass;
-            this.idsub = idSub;
-        }
-
-        protected Key() {}
-
-        public String getClassId() {
-            return idclass;
-        }
-
-        public String getSubjectId() {
-            return idsub;
-        }
-
-        public Long getIduser() {
-            return iduser;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            UserSemester.Key key = (UserSemester.Key) o;
-            return Objects.equals(iduser, key.iduser) && Objects.equals(idclass, key.idclass) && Objects.equals(idsub, key.idsub);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash( iduser, idclass, idsub);
-        }
+        return Objects.hash(id);
     }
 }
