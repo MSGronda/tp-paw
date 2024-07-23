dbcd60f432aac40c6e4e928d21a44ef082ddae91
Agustín Morantes <amorantes@itba.edu.ar>
2024-07-23 07:28:14 -0300

    refactor: remove old jsp, js and css

--- webapp/src/main/webapp/js/sanitizing-utils.js
+++
@@ -1,3 +0,0 @@
-function un_sanitize(sanitized){
-    return sanitized.replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&quo;t/g, "\"").replace(/&#x27;/g, "<").replace(/&#x2F;/g, "/")
-}
